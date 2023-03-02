61b3a7253bcfefca5091219f23e251f2164f389b
Sheleoni <sheleoni@gmail.com>
2023-03-02 21:39:16 +0900

    chore: clear non-existent image file paths with regex

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,32 +1,50 @@
 import React from 'react';
 import styles from './Question.module.css'
 
-const questionImages = [
-    {
-        src: "src/assets/あ_stroke.gif",
-        letter: "あ"
-    }, {
-        src: "src/assets/あ_stroke.gif",
-        letter: "あ"
-    }, {
-        src: "src/assets/あ_stroke.gif",
-        letter: "あ"
-    }, {
-        src: "src/assets/あ_stroke.gif",
-        letter: "あ"
-    }, {
-        src: "src/assets/あ_stroke.gif",
-        letter: "あ"
-    }
-];
+function Question({question, setQuestion, answer, getRandomLetter}) {
 
-function Question({answer}) {
+    // const displayImage = () => {
+    //     return <img className={styles.questionImage}
+    //                 src={question.hiraganaImgURL}
+    //                 alt={question.hiragana}/>
+    // }
+
+
+    const imgPath = question.hiraganaImgURL;
+
+    fetch(imgPath)
+        .then(response => {
+            if (response.ok) {
+                console.log('Image is available online');
+            } else {
+                console.error('Image is not available online');
+            }
+        })
+        .catch(error => {
+            console.error('Error checking image:', error);
+        });
+    const getNextQuestion = () => {
+        console.log("getting next question!")
+        const newQuestion = getRandomLetter();
+        setQuestion({
+            hiragana: newQuestion.hiragana,
+            hiraganaImgURL: newQuestion.hiraganaImgURL,
+            romaji: newQuestion.romaji
+        });
+    };
+
+
     return (
-        <section className={styles.question}>
+        <section className={styles.question}
+                 onClick={() => {
+                     getNextQuestion()
+                 }}>
+
             <img className={styles.questionImage}
-                 src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1676767333/hiragana_game/%E3%81%82_stroke_hgeopi.gif"
-                 alt="あ"/>
-            current answer is : {answer}
+                 src={question.hiraganaImgURL}
+                 alt={question.hiragana}/>
+
+            current answer is : {question.hiragana}
         </section>
     );
 }
