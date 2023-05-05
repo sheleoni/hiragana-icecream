7eb7be8d070d9750ea23e7101f68bab84d68dcae
Sheleoni <sheleoni@gmail.com>
2023-05-06 00:34:08 +0900

    feat: generate questionPool by filtering modal

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,40 +1,61 @@
 import React from 'react';
-import styles from './Question.module.css'
+import styles from './Question.module.css';
 import {LevelFilterContext} from "../../contexts/LevelFilterContext.jsx";
-import questionPool from "../Game/questionPool.js";
+import rowLetters from "../Game/rowLetters.js";
 
 function Question({question, answer, updateQuestion, choiceData}) {
-
     const imgPath = question.hiraganaImgURL;
-    const {levelFilter, setLevelFilter} = React.useContext(LevelFilterContext);
+    const {levelFilter} = React.useContext(LevelFilterContext);
+    const [questionPool, setQuestionPool] = React.useState([]);
 
-    console.log('Level Filter from Question component:')
-    console.log(levelFilter)
-
-    console.log(questionPool, 'the questionPool from Question.jsx')
     const questionImage = () => {
         if (imgPath) {
             return (
                 <img className={styles.questionImage}
                      src={question.hiraganaImgURL}
-                     alt={question.hiragana}/>
-            )
+                     alt={question.hiragana}
+                />
+            );
         } else {
             return (
                 <p className={styles.questionWord}>
                     {question.hiragana}
-                </p>);
+                </p>
+            );
         }
     }
 
+    React.useEffect(() => {
+        const filterCharacters = (characterList) => {
+            let filteredCharacters = [];
+            for (const characterSet in characterList) {
+                const characterSetList = characterList[characterSet];
+                for (const character in characterSetList) {
+                    if (characterSetList[character] === true) {
+                        filteredCharacters.push(character);
+                    }
+                }
+            }
+            return filteredCharacters;
+        }
+
+        const filteredCharacters = filterCharacters(levelFilter);
+        let updatedQuestionPool = [];
+        filteredCharacters.forEach((row) => {
+            const lettersInRow = rowLetters[`${row}行`];
+            updatedQuestionPool = updatedQuestionPool.concat(lettersInRow);
+        });
+
+        setQuestionPool(updatedQuestionPool);
+    }, [levelFilter]);
 
     return (
-        <section className={styles.question}
-                 onClick={() => {
-                     updateQuestion(choiceData)
-                 }}>
-            <p>current answer is : {question.hiragana}</p>
-            <br/>question pool size: {questionPool.length}
+        <section
+            className={styles.question}
+            onClick={() => {
+                updateQuestion(choiceData);
+            }}
+        >
             {questionImage()}
         </section>
     );
