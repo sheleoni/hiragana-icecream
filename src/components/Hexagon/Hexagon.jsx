8ce26fcfaa4e7425f74073410f3a6912a71d27ba
Sheleoni <sheleoni@gmail.com>
2023-05-11 00:24:21 +0900

    feat: increment tide levels when the player answers the correct answer >=2 times! (or gets it right the first time the character appears in the game session

--- src/components/Hexagon/Hexagon.jsx
+++ src/components/Hexagon/Hexagon.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import styles from './Hexagon.module.css';
 
-function Hexagon({choiceLetter}) {
+function Hexagon({choiceLetter, tideLevel}) {
+    const tideLevelToPixels = (tideLevel) => {
+        /* Tide levels corresponding to CSS margin values:
+        *
+        * tideLevel 0 (barely visible crests) -> 120px
+        * tideLevel 1 -> 96px
+        * tideLevel 2 -> 72px
+        * tideLevel 3 -> 48px
+        * tideLevel 4 -> 24px
+        * tideLevel 5 (fully filled) -> 10px
+        *
+        * */
+
+        switch (tideLevel) {
+            case 0:
+                return 120;
+            case 1:
+                return 96;
+            case 2:
+                return 72;
+            case 3:
+                return 48;
+            case 4:
+                return 24;
+            case 5:
+                return 10;
+        }
+    }
+
+    const tideMarginPixels = tideLevelToPixels(tideLevel[choiceLetter]);
     return (
         <>
             <span className={styles.hexagonWrapper}>
@@ -21,17 +50,21 @@ function Hexagon({choiceLetter}) {
                     />
                     <div className={styles.mask1}>
                         <div className={styles.waveWrapper}>
+                            {/* continuous wave images */}
                             <img
                                 className={styles.movingWave}
-                                src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1676733062/hiragana_game/wave_red_big_qtvsld.png"
+                                style={{marginBlockStart: `${tideMarginPixels}px`}}
+                                src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1683549981/wave_red_big_taller1_j1nqs4.png"
                             />
                             <img
                                 className={styles.movingWave}
-                                src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1676733062/hiragana_game/wave_red_big_qtvsld.png"
+                                style={{marginBlockStart: `${tideMarginPixels}px`}}
+                                src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1683549981/wave_red_big_taller1_j1nqs4.png"
                             />
                             <img
                                 className={styles.movingWave}
-                                src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1676733062/hiragana_game/wave_red_big_qtvsld.png"
+                                style={{marginBlockStart: `${tideMarginPixels}px`}}
+                                src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1683549981/wave_red_big_taller1_j1nqs4.png"
                             />
                         </div>
                     </div>
