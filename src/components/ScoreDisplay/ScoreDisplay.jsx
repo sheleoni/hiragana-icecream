3ac2196074192e9837de6bfbdbe437d6678c8021
Sheleoni <sheleoni@gmail.com>
2023-05-23 02:08:59 +0900

    feat: hover on icecream scoop to show vocabulary

--- src/components/ScoreDisplay/ScoreDisplay.jsx
+++ src/components/ScoreDisplay/ScoreDisplay.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './ScoreDisplay.module.css';
+// import {Tooltip, TooltipTrigger, TooltipContent, TooltipArrow} from '@radix-ui/react-tooltip';
+import * as Tooltip from '@radix-ui/react-tooltip';
 
 function ScoreDisplay({score, iceCreamScoops, iceCreamChoiceData}) {
 
@@ -12,15 +14,37 @@ function ScoreDisplay({score, iceCreamScoops, iceCreamChoiceData}) {
         <p style={{marginBlockEnd: 50}}>Your score is:{' '}{totalScore}</p>
         <ul className={styles.iceCreamContainer}>
             <li>
+                {/* Icecream cone*/}
                 <img
                     className={styles.iceCreamCone}
                     src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1684298737/hiragana_game/icecream_scoops/Cone_ncndkh.png"/>
             </li>
             {iceCreamScoops.map((icecream, index) => {
                 return (
-                    <li key={index} className={styles.iceCreamListContainer} style={{zIndex: index + 1}}>
-                        <img className={styles.iceCreamScoop} alt={`${icecream.name}`} src={icecream.imgURL}/>
-                    </li>
+                    <>
+
+
+                        <li key={index} className={styles.iceCreamListContainer} style={{zIndex: index + 1}}>
+                            <Tooltip.Provider delayDuration={30}>
+                                <Tooltip.Root>
+                                    <Tooltip.Trigger asChild>
+                                        {/*image of scoop*/}
+                                        {/*TODO: confirm if <button> tag is recommended over <img> tag in radix*/}
+                                        <img className={styles.iceCreamScoop} alt={`${icecream.name}`}
+                                             src={icecream.imgURL}/>
+                                    </Tooltip.Trigger>
+                                    <Tooltip.Portal>
+                                        <Tooltip.Content sideOffset={-15} className={styles.iceCreamHoverPanel}>
+                                            {icecream.name}
+                                            <Tooltip.Arrow/>
+                                        </Tooltip.Content>
+                                    </Tooltip.Portal>
+                                </Tooltip.Root>
+                            </Tooltip.Provider>
+
+
+                        </li>
+                    </>
                 )
             })}
         </ul>
