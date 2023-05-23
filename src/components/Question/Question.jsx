b7dc8952e2efda04c439a95b91f948bf0152be9e
Sheleoni <sheleoni@gmail.com>
2023-05-23 18:02:38 +0900

    refactor: cleaned up unused code

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -5,20 +5,11 @@ function Question({question, sampleQuestion, getRandomQuestion}) {
     const imgPath = null;
 
     const questionImage = () => {
-        if (false) {
-            return (
-                <img className={styles.questionImage}
-                     src={question.hiraganaImgURL}
-                     alt={question.hiragana}
-                />
-            );
-        } else {
-            return (
-                <p className={styles.questionWord}>
-                    {sampleQuestion}
-                </p>
-            );
-        }
+        return (
+            <p className={styles.questionWord}>
+                {sampleQuestion}
+            </p>
+        );
     }
     return (
         <section
