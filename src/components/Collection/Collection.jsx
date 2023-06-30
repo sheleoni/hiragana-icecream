84262eab06d1cc16fc9685e67c279ffaaa1687af
Sheleoni <sheleoni@gmail.com>
2023-06-30 23:44:49 +0900

    refactor: comment out unused styles

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from './Collection.css';
-import * as Tooltip from '@radix-ui/react-tooltip';
+// import styles from './Collection.css';
+// import * as Tooltip from '@radix-ui/react-tooltip';
 
 function Collection () {
     return (
